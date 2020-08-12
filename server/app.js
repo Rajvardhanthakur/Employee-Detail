fbaf330d52cf0930fa9580fdd7134153ccdf2840
Rajvardhan <rajthakur3619@gmail.com>
2020-08-13 01:39:34 +0530

    establishing connection to mongodb

--- server/app.js
+++ server/app.js
@@ -2,10 +2,25 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+require('./Employee')
 const PORT = 3000
 
-
+const Employee = mongoose.model("employee")
 const mongoUri = "mongodb+srv://Raj:njxJ9Q33rgCHsGmk@cluster0.8vgti.mongodb.net/<dbname>?retryWrites=true&w=majority"
+
+
+mongoose.connect(mongoUri, {
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+})
+
+mongoose.connection.on('connected',()=>{
+    console.log("Connection established")
+})
+
+mongoose.connection.on('error',(err)=>{
+    console.log("Error : ",err)
+})
 
 app.get('/',(req,res)=>{
     res.send("Server started")
