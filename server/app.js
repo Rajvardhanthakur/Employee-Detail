ef39ae3a95cf3ddfc8a46a669a3f89a2d7f00eb7
Rajvardhan <rajthakur3619@gmail.com>
2020-08-13 14:57:54 +0530

    updating and deleting and getting records

--- server/app.js
+++ server/app.js
@@ -5,6 +5,9 @@ const mongoose = require('mongoose')
 require('./Employee')
 const PORT = 3000
 
+app.use(bodyParser.json())
+
+
 const Employee = mongoose.model("employee")
 const mongoUri = "mongodb+srv://Raj:njxJ9Q33rgCHsGmk@cluster0.8vgti.mongodb.net/<dbname>?retryWrites=true&w=majority"
 
@@ -23,7 +26,58 @@ mongoose.connection.on('error',(err)=>{
 })
 
 app.get('/',(req,res)=>{
-    res.send("Server started")
+    Employee.find({}).then(data=>{
+        console.log(data)
+        res.send(data)
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+})
+
+app.post('/send-data',(req, res)=>{
+    const employee = new Employee({
+        name:req.body.name,
+        email:req.body.email,
+        phone:req.body.phone,
+        picture:req.body.picture,
+        salary:req.body.salary,
+        position:req.body.position
+    })
+    employee.save().then(data => {
+        console.log(data)
+        res.send("successfully posted")
+    }).catch(err=>{
+        console.log(err)
+    })
+})
+
+
+app.post('/delete',(req, res)=>{
+    Employee.findByIdAndRemove(req.body.id)
+    .then(data=>{
+        console.log(data)
+        res.send(data)
+    }).catch(err =>{
+        console.log(err)
+    })
+})
+
+app.post('/update',(req,res)=>{
+    Employee.findByIdAndUpdate(req.body.id,{
+        name:req.body.name,
+        email:req.body.email,
+        phone:req.body.phone,
+        picture:req.body.picture,
+        salary:req.body.salary,
+        position:req.body.position
+    }).then(data=>{
+        console.log(data)
+        res.send(data)
+    })
+    .catch(err=>{
+        console.log("server running")
+    })
 })
 
 app.listen(PORT, ()=>{
